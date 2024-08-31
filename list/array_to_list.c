871fd24c36fb1679ce8183855a5122c890a0ff2e
Ycaro02 <Ycaro02@hotmail.fr>
2024-08-31 17:56:04 +0200

    Fix array_to_list cpy

--- list/array_to_list.c
+++ list/array_to_list.c
@@ -6,7 +6,7 @@ t_list *array_to_list(void *arr, u16 size, u16 size_of_elem) {
 
 	for (int i = 0; i < size; i++) {
 		node = ft_calloc(1, size_of_elem);
-		ft_memcpy(node, arr + i, size_of_elem);
+		ft_memcpy(node, arr +(i * size_of_elem), size_of_elem);
 		ft_lstadd_back(&lst, ft_lstnew(node));
 	}
 	return (lst);
